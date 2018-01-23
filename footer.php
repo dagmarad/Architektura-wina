a272df747d62ca87345df2dab6a460f084ae2e00
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2018-01-23 13:02:48 +0100

    change footer

--- footer.php
+++ footer.php
@@ -18,54 +18,7 @@ $boxedornot = education_park_boxedornot();
 	</div><!-- #content -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<div class="footer-widget">
-				<div class="container">
-					<div class="row">
-
-						<div class="col-md-4 col-sm-12 pad0 foot-bor">
-							<?php
-								if ( is_active_sidebar( 'footer-1' ) ) {
-									dynamic_sidebar( 'footer-1' );
-								}
-								else{
-									if(is_user_logged_in() && current_user_can('edit_theme_options') ){
-										echo '<h6 class="text-center"><a href="'.esc_url(admin_url("customize.php")).'"><i class="fa fa-plus-circle"></i>'.esc_html__('Assign a Widget', 'education-park').'</a></h6>';
-									}
-								}
-							?>
-						</div>
-
-						<div class="col-md-4 col-sm-12 pad0 foot-bor">
-							<?php
-								if ( is_active_sidebar( 'footer-2' ) ) {
-									dynamic_sidebar( 'footer-2' );
-								}
-								else{
-									if(is_user_logged_in()&& current_user_can('edit_theme_options') ){
-										echo '<h6 class="text-center"><a href="'.esc_url(admin_url("customize.php")).'"><i class="fa fa-plus-circle"></i>'.esc_html__('Assign a Widget', 'education-park').'</a></h6>';
-									}
-								}
-							?>
-						</div>
-
-						<div class="col-md-4 col-sm-12 pad0 foot-bor br0">
-							<?php
-								if ( is_active_sidebar( 'footer-3' ) ) {
-									dynamic_sidebar( 'footer-3' );
-								}
-								else{
-									if(is_user_logged_in()&& current_user_can('edit_theme_options')  ){
-										echo '<h6 class="text-center"><a href="'.esc_url(admin_url("customize.php")).'"><i class="fa fa-plus-circle"></i>'.esc_html__('Assign a Widget', 'education-park').'</a></h6>';
-									}
-								}
-							?>
-						</div>
-						<!-- End Footer Widget Columns -->
-
-					</div>
-				</div>
-
-	        </div>
+		
 	        <!-- Footer Widgets -->
 
 	        <div class="copyright clearfix">
@@ -77,13 +30,18 @@ $boxedornot = education_park_boxedornot();
 			    <div class="container pad0">
 
 				  	<div class="copyright-content">
-				        <p class="text-right">
-				        	<?php $copyright_link = esc_url('https://yudleethemes.com/'); ?>
-				        		<?php esc_html_e('Theme by ', 'education-park');?>
-		        				<a href="<?php echo esc_url($copyright_link); ?>" target="_blank">
-		        					 <?php echo esc_html__( 'Yudlee Themes','education-park' ); ?>
-		        				</a>
-						</p>
+						<div class="address-content">	
+							<p><strong>--------- Adres</strong></p>
+							<p>ul. Szpitalna 17</p>
+						</div>
+						<div class="address-content">						
+							<p><strong>--------- Telefon</strong></p>
+						<p>665-749-144</p>
+						</div>	
+						<div class="address-content">	
+						<p><strong>--------- E-mail</strong></p>
+						<p>architektura-wina@gmail.com</p>
+						</div>
 				  	</div>
 
 			  	</div>
